356dbdbc603c47933957f19e69db5616e599ef9a
Pedro DS <pedroperrenoudds@gmail.com>
2025-02-02 17:12:07 -0300

    Primeiro Commit: Subindo projeto existente para o GitHub

---
+++ ConsoleState_RPG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleState_RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Desenvolvendo um RPG ");
+        }  
+     }
+}
